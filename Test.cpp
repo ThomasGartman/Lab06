0059b78399e59cc6bf0e487747658437cb7fba02
Thomas Gartman <thomasgartman@ku.edu>
2018-11-02 15:59:27 -0500

    Added 7 Tests, appears to be working.

---
+++ Test.cpp
@@ -0,0 +1,94 @@
+/**
+* @author Thomas Gartman
+* @file Test.cpp
+* @date November 2nd, 2018
+* @brief Test class for LinkedListOfInts
+*/
+
+#include <iostream>
+#include <vector>
+#include "Test.h"
+Test::Test()
+{
+
+}
+
+void Test::runTests(LinkedListOfInts list)
+{
+  std::cout << "Test 1: isEmpty() on empty list: ";
+  resultPrint(testIsEmpty(list, true));
+
+  std::cout << "Test 2: size() on empty list: ";
+  resultPrint(testSize(list, 0));
+
+  std::cout << "Test 3: AddFront(1) on empty list: ";
+  resultPrint(testAddFront(list, 1));
+
+  std::cout << "Test 4: isEmpty() on non empty list: ";
+  resultPrint(testIsEmpty(list, false));
+
+  std::cout << "Test 5: size() list of size 1: ";
+  resultPrint(testSize(list, 1));
+
+  std::cout << "Test 6: AddFront(2) on non-empty list: ";
+  resultPrint(testAddFront(list, 2));
+
+  std::cout << "Test 7: RemoveFront() on non-empty list: ";
+  resultPrint(testRemoveFront(list, false, 1));
+}
+
+bool Test::testIsEmpty(LinkedListOfInts list, bool expected)
+{
+  if(list.isEmpty())
+  {
+    return expected == true;
+  }
+  return expected == false;
+}
+
+bool Test::testAddFront(LinkedListOfInts list, int expectedValue)
+{
+  list.addFront(expectedValue);
+  std::vector<int> tempVector = list.toVector();
+  if(tempVector.front() == expectedValue)
+  {
+    return true;
+  }
+  return false;
+}
+
+bool Test::testSize(LinkedListOfInts list, int expectedSize)
+{
+  if(list.size() == expectedSize)
+  {
+    return true;
+  }
+  return false;
+}
+
+bool Test::testRemoveFront(LinkedListOfInts list, bool expected, int expectedValue)
+{
+  //empty list behavior
+  if(!list.removeFront())
+  {
+    return expected;
+  }
+  std::vector<int> tempVector = list.toVector();
+  if(tempVector.front() == expectedValue)
+  {
+    return true;
+  }
+  return false;
+}
+
+void Test::resultPrint(bool result)
+{
+  if(result)
+  {
+    std::cout << m_green << "PASSED" << m_normal << std::endl;
+  }
+  else
+  {
+    std::cout << m_red << "FAILED" << m_normal << std::endl;
+  }
+}
---
+++ Test.h
@@ -0,0 +1,72 @@
+/**
+* @author Thomas Gartman
+* @file Test.h
+* @date November 2nd, 2018
+* @brief header for test class
+*/
+
+#ifndef TEST_H
+#define TEST_H
+
+#include "LinkedListOfInts.h"
+
+class Test
+{
+  public:
+    /**
+    * @pre None
+    * @post Test Class initialized
+    */
+    Test();
+
+    /**
+    * @pre Linked List Exists
+    * @post Test Results known
+    * @return None
+    */
+    void runTests(LinkedListOfInts list);
+
+    /**
+    * @pre Linked List Exists
+    * @post None
+    * @return None
+    */
+    void resultPrint(bool result);
+
+    /**
+    * @pre Linked List Exists
+    * @post None
+    * @return True if Test Passed, False Otherwise
+    */
+    bool testIsEmpty(LinkedListOfInts list, bool expected);
+
+    /**
+    * @pre Linked List Exists
+    * @post None
+    * @return True if Test Passed, False Otherwise
+    */
+    bool testAddFront(LinkedListOfInts list, int expectedValue);
+
+    /**
+    * @pre Linked List Exists
+    * @post None
+    * @return True if Test Passed, False Otherwise
+    */
+    bool testSize(LinkedListOfInts list, int expectedSize);
+
+    /**
+    * @pre Linked List Exists
+    * @post None
+    * @return True if Test Passed, False Otherwise
+    */
+    bool testRemoveFront(LinkedListOfInts list, bool expected, int expectedValue);
+
+  private:
+    //Credit for this approach goes to http://www.cplusplus.com/forum/beginner/1640/ user jsmith
+    //m_green and m_red are my definitions based upon m_normal from above
+    char m_green[8] = {0x1b, '[', '1', ';', '3', '2', 'm',0};
+    char m_red[8] = {0x1b, '[', '1', ';', '3', '1', 'm',0};
+    char m_normal[8] = {0x1b, '[', '0', ';', '3', '9', 'm', 0};
+};
+
+#endif
